056a978da6ff252a3aa49746e5aeeb2ab333d321
Andrew Bastien <andrew.bastien@ericsson.com>
2013-12-04 12:39:28 -0800

    Add sudo_version fact

---
+++ lib/facter/sudo_version.rb
@@ -0,0 +1,45 @@
+# sudo_version
+
+if File.exists? '/opt/quest/bin/sudo'
+  path = '/opt/quest/bin/sudo'
+  questsudo = true
+else
+  path = '/usr/bin/sudo'
+  questsudo = false
+end
+
+Facter.add("sudo_version") do
+  setcode do
+    response = ''
+
+    # Which sudo is used?
+    display_name = ''
+
+    if /\/quest\// =~ path then
+      # quest-sudo is used!
+      regexp = /^Sudo version +(\S+)q\d+$/
+    else
+      # sudo is used!
+      regexp = /^Sudo version +(\S+)$/
+    end
+
+    # Check if path is valid
+    if File.exist?( path ) then
+      cmd = path + ' -V'
+      str = Facter::Util::Resolution.exec( cmd )
+      if $?.exitstatus == 0 and regexp =~ str then
+        response = Regexp.last_match(1)
+
+      end
+    end
+
+    response # Return
+  end
+end
+
+Facter.add("quest_sudo") do
+  setcode do
+    questsudo
+  end
+end
+
