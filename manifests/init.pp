1e957d8a3fa1d0d48ebb483c9ab295453524506f
Phil Friderici <phil.friderici@elits.com>
2022-09-08 14:16:03 +0000

    Remove obsolete validations

--- manifests/init.pp
+++ manifests/init.pp
@@ -126,84 +126,6 @@ class sudo (
   String[1]                                      $always_query_group_plugin = 'USE_DEFAULTS',
   Boolean                                        $hiera_merge_sudoers       = false,
 ) {
-  if is_string($package_manage) {
-    $package_manage_real = str2bool($package_manage)
-  } else {
-    $package_manage_real = $package_manage
-  }
-  if is_string($sudoers_manage) {
-    $sudoers_manage_real = str2bool($sudoers_manage)
-  } else {
-    $sudoers_manage_real = $sudoers_manage
-  }
-  if is_string($config_dir_purge) {
-    $config_dir_purge_real = str2bool($config_dir_purge)
-  } else {
-    $config_dir_purge_real = $config_dir_purge
-  }
-  if is_string($requiretty) {
-    $requiretty_real = str2bool($requiretty)
-  } else {
-    $requiretty_real = $requiretty
-  }
-  if is_string($visiblepw) {
-    $visiblepw_real = str2bool($visiblepw)
-  } else {
-    $visiblepw_real = $visiblepw
-  }
-  if is_string($always_set_home) {
-    $always_set_home_real = str2bool($always_set_home)
-  } else {
-    $always_set_home_real = $always_set_home
-  }
-  if is_string($envreset) {
-    $envreset_real = str2bool($envreset)
-  } else {
-    $envreset_real = $envreset
-  }
-  if is_string($root_allow_all) {
-    $root_allow_all_real = str2bool($root_allow_all)
-  } else {
-    $root_allow_all_real = $root_allow_all
-  }
-  if is_string($includedir) {
-    $includedir_real = str2bool($includedir)
-  } else {
-    $includedir_real = $includedir
-  }
-  if is_string($include_libsudo_vas) {
-    $include_libsudo_vas_real = str2bool($include_libsudo_vas)
-  } else {
-    $include_libsudo_vas_real = $include_libsudo_vas
-  }
-
-  if is_string($hiera_merge_sudoers) {
-    $hiera_merge_sudoers_bool = str2bool($hiera_merge_sudoers)
-  } else {
-    $hiera_merge_sudoers_bool = $hiera_merge_sudoers
-  }
-
-  # Validate params
-  validate_bool($package_manage_real)
-  validate_bool($sudoers_manage_real)
-  validate_bool($config_dir_purge_real)
-  validate_bool($requiretty_real)
-  validate_bool($visiblepw_real)
-  validate_bool($always_set_home_real)
-  validate_bool($envreset_real)
-  validate_bool($root_allow_all_real)
-  validate_bool($includedir_real)
-  validate_bool($include_libsudo_vas_real)
-  validate_bool($hiera_merge_sudoers_bool)
-  validate_absolute_path($config_dir)
-  validate_absolute_path($config_file)
-  if $package_adminfile != undef {
-    validate_absolute_path($package_adminfile)
-  }
-  if $sudoers_manage_real == true {
-    validate_array($envkeep)
-    validate_string($secure_path)
-  }
   # Only works with sudo >= 1.8
   if $libsudo_vas_location == 'USE_DEFAULTS' {
     if $facts['os']['architecture'] =~ /amd64|x86_64/ {
@@ -214,15 +136,12 @@ class sudo (
   } else {
     $libsudo_vas_location_real = $libsudo_vas_location
   }
-  if $include_libsudo_vas_real == true {
-    validate_absolute_path($libsudo_vas_location_real)
-  }
 
   # Sudo 1.8.15 introduced a new Defaults option 'always_query_group_plugin'.
   # This option is required in >= 1.8.15 if you want sudo to automatically do
   # lookups through group_plugins.
   if $always_query_group_plugin == 'USE_DEFAULTS' {
-    if (versioncmp("${facts['sudo_version']}", '1.8.15') >= 0) and $include_libsudo_vas_real == true { # lint:ignore:only_variable_string
+    if (versioncmp("${facts['sudo_version']}", '1.8.15') >= 0) and $include_libsudo_vas == true { # lint:ignore:only_variable_string
       $always_query_group_plugin_real = true
     } else {
       $always_query_group_plugin_real = false
@@ -231,7 +150,7 @@ class sudo (
     $always_query_group_plugin_real = $always_query_group_plugin
   }
 
-  if $package_manage_real == true {
+  if $package_manage == true {
     package { 'sudo-package':
       ensure    => $package_ensure,
       name      => $package,
@@ -240,7 +159,7 @@ class sudo (
     }
   }
 
-  if $sudoers_manage_real == true {
+  if $sudoers_manage == true {
     file { $config_file:
       owner   => $config_file_owner,
       group   => $config_file_group,
@@ -252,18 +171,17 @@ class sudo (
       owner   => 'root',
       group   => $config_dir_group,
       mode    => $config_dir_mode,
-      recurse => $config_dir_purge_real,
-      purge   => $config_dir_purge_real,
+      recurse => $config_dir_purge,
+      purge   => $config_dir_purge,
     }
 
     # Only works with sudo >= 1.7.2
     if $sudoers != undef {
-      if $hiera_merge_sudoers_bool {
+      if $hiera_merge_sudoers {
         $sudoers_real = hiera_hash(sudo::sudoers)
       } else {
         $sudoers_real = $sudoers
       }
-      validate_hash($sudoers_real)
       create_resources('sudo::fragment',$sudoers_real)
     }
   }
